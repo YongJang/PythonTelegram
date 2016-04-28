a18f417c17105402cbb8e9f66e13c8315791a615
YongJang <yj_430@naver.com>
2016-04-29 00:50:18 +0900

    a

---
+++ examples/a.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import urllib3.request
+
+html = urllib3.request.urlopen('http://www.nlotto.co.kr/common.do?method=main&#8217;')
+
+soup = BeautifulSoup(html)
+
+hoi = soup.find("span", id="lottoDrwNo")
+
+numbers=[]
+
+for n in range(1,7):
+    strV ="drwtNo" + str(n)
+    first = soup.find('img', id=strV)['alt']
+    numbers.append(first)
+
+bonus = soup.find('img', id="bnusNo")['alt']
+
+print('Lotto numbers')
+print(hoi.string + "results")
+print(" ".join(numbers))
+print('Bonus_number: '+bonus)
