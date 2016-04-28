9d1193723080afe8ed4baf1c83a5da32c88ef5c8
YongJang <yj_430@naver.com>
2016-04-29 01:22:46 +0900

    echobot 수정

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -7,13 +7,29 @@ API_TOKEN = '207944330:AAGdpOvswmHangYooE8wBEf1p-vYP2skyL0'
 
 bot = telebot.TeleBot(API_TOKEN)
 
+#LOTTO#
+from bs4 import BeautifulSoup
+import urllib.request
+
+html = urllib.request.urlopen('http://www.nlotto.co.kr/common.do?method=main&#8217;')
+
+soup = BeautifulSoup(html)
+
+hoi = soup.find("span", id="lottoDrwNo")
+
+numbers=[]
+
+for n in range(1,7):
+    strV ="drwtNo" + str(n)
+    first = soup.find('img', id=strV)['alt']
+    numbers.append(first)
+
+bonus = soup.find('img', id="bnusNo")['alt']
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Lotto numbers\n"+hoi.string + "results"+" ".join(numbers)+'Bonus_number: '+bonus)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
