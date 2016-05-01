8d386adba1a6cba9978cddba175303bcceb97a84
YongJang <yj_430@naver.com>
2016-05-01 15:26:26 +0900

    zz

--- examples/c.py
+++ examples/c.py
@@ -1,22 +1,41 @@
 #-*- encoding: utf-8 -*-
 import requests as rs
-import bs4
+import bs4 import BeautifulSoup
+import urllib2
 import time
 
 def getArticle() :
     medium_technology_url = 'https://medium.com/browse/b99480981476'
 
-    response = rs.get(medium_technology_url)
+    content = urllib2.urlopen(medium_technology_url).read()
 
-    html_content = response.text.encode(response.encoding);
+    soup = BeautifulSoup(content)
 
-    navigator = bs4.BeautifulSoup(html_content)
+    print soup.prettify()
 
-    articleHeader = navigator.find_all('h3')
+    print h3
+    >> 'h3'? A year and a half with Alexa
 
-    for keyword in enumerate(articleHeader):
-		resultText = '%s'%(keyword.encode('utf-8'))
-        print resultText.decode('utf-8').encode('utf-8')
+    print soup.h3.string
+    >> ? A year and a half with Alexa
+
+    print soup
+    print soup.a
+    A year and a half with Alexa
+
+    #response = rs.get(medium_technology_url)
+
+    #html_content = response.text.encode(response.encoding);
+
+    #navigator = bs4.BeautifulSoup(html_content)
+
+    #articleHeader = navigator.find_all('h3')
+
+    #for keyword in enumerate(articleHeader):
+	#	resultText = '%s'%(keyword.encode('utf-8'))
+    #    print resultText
+
+    #print ''
 
 def Medium_Technology() :
     getArticle()
