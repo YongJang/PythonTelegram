f2ec36c8d39f663a1f48cf342a3d1ba0f0b35431
alsqls <tpgml9247@gmail.com>
2016-05-01 18:21:21 +0900

    eventtt

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -5,14 +5,8 @@ def getPost() :
 
     html = Request('http://www.onoffmix.com/event?s=IT', headers={'User-Agent':'Mozilla/5.0'})
     webpage = urlopen(html).read()
-    soup = BeautifulSoup(content)
-"""
-    all_links = soup.find_all("a")
-    for link in all_links:
-        print (link.get("href"))
-        """
-    #hoi = soup.find_all('ul', class_ = "todayEvent")
-    event =soup.find('a', class_="eventLink")
+    soup = BeautifulSoup(webpage)
+    event = soup.find('a', class_="eventLink")
     for i in event:
         print (i.get("title"))
 
