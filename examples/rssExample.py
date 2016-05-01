888ad21fec8b9036ec117926903276386e514159
YongJang <yj_430@naver.com>
2016-05-02 01:03:00 +0900

    eee

---
+++ examples/rssExample.py
@@ -0,0 +1,23 @@
+import feedparser
+
+d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=%C0%CE%B0%F8%C1%F6%B4%C9&field=0')
+
+print (d['feed']['title'])
+
+print (d['feed']['link'])
+
+print d.feed.subtitle
+
+print len(d['entries'])
+
+print d['entries'][0]['title']
+
+print d.entries[0]['link']
+
+for post in d.entries:
+    print post.title + ":" + post.link + "
+    "
+
+print d.version
+
+print d.headers.get('content-type')
