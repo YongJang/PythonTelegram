bcadbc0840ab6e269bfd8d99ea31d6565d282816
YongJang <yj_430@naver.com>
2016-05-01 15:57:33 +0900

    ccc

--- examples/c.py
+++ examples/c.py
@@ -1,41 +1,22 @@
-#-*- encoding: utf-8 -*-
-import requests as rs
-import bs4 import BeautifulSoup
-import urllib2
-import time
+from bs4 import BeautifulSoup
+import urllib.request
 
 def getArticle() :
-    medium_technology_url = 'https://medium.com/browse/b99480981476'
+    html = urllib.request.urlopen('https://medium.com/browse/b99480981476;')
 
-    content = urllib2.urlopen(medium_technology_url).read()
+    soup = BeautifulSoup(html)
 
-    soup = BeautifulSoup(content)
+    hoi = soup.find("h3")
 
-    print soup.prettify()
+    numbers=[]
 
-    print h3
-    >> 'h3'? A year and a half with Alexa
+    for n in range(1,4):
+        strV = str(n)
+        numbers.append(strV)
 
-    print soup.h3.string
-    >> ? A year and a half with Alexa
-
-    print soup
-    print soup.a
-    A year and a half with Alexa
-
-    #response = rs.get(medium_technology_url)
-
-    #html_content = response.text.encode(response.encoding);
-
-    #navigator = bs4.BeautifulSoup(html_content)
-
-    #articleHeader = navigator.find_all('h3')
-
-    #for keyword in enumerate(articleHeader):
-	#	resultText = '%s'%(keyword.encode('utf-8'))
-    #    print resultText
-
-    #print ''
+    print('!!')
+    print(hoi.string + "results")
+    print(" ".join(numbers))
 
 def Medium_Technology() :
     getArticle()
