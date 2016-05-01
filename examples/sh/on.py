b9b38e3631687777f664c83c50e5f931a14c25fe
alsqls <tpgml9247@gmail.com>
2016-05-01 17:21:22 +0900

    on test

---
+++ examples/sh/on.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+def getPost() :
+
+    html = Request('http://www.onoffmix.com/event?s=IT', headers={'User-Agent':'Mozilla/5.0'})
+    webpage = urlopen(html).read()
+
+    soup = BeautifulSoup(webpage)
+
+    hoi = soup.find_all("contentBox todayEveryArea")
+
+    for n in range(0,len(hoi)):
+        print(hoi[n].string)
+
+def Medium_Technology() :
+    getPost()
+
+if __name__ == '__main__' :
+    Medium_Technology()
