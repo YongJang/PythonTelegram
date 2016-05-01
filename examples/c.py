2e66217967a4db044b283a89edc8ed966e4980ea
YongJang <yj_430@naver.com>
2016-05-01 16:14:00 +0900

    zz

--- examples/c.py
+++ examples/c.py
@@ -3,21 +3,20 @@ from urllib.request import Request, urlopen
 
 def getArticle() :
 
-    html = Request('https://medium.com/browse/b99480981476;', headers={'User-Agent':'Mozilla/5.0'})
+    html = Request('https://medium.com/browse/b99480981476', headers={'User-Agent':'Mozilla/5.0'})
     webpage = urlopen(html).read()
 
     soup = BeautifulSoup(webpage)
 
-    hoi = soup.find("h3")
+    hoi[] = soup.find_all("h3")
 
     numbers=[]
 
     for n in range(1,4):
-        strV = str(n)
+        print(hoi[n].string)
         numbers.append(strV)
 
-    print('!!')
-    print(hoi.string + "results")
+
     print(" ".join(numbers))
 
 def Medium_Technology() :
