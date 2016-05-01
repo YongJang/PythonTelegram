79001812766544f25818397f9336822b0ecc8eb7
alsqls <tpgml9247@gmail.com>
2016-05-01 23:24:35 +0900

    전공별

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -3,10 +3,10 @@ from urllib.request import Request, urlopen
 
 def getPost() :
 
-    html = Request('http://www.onoffmix.com/event?s=IT', headers={'User-Agent':'Mozilla/5.0'})
+    html = Request('http://www.jobkorea.co.kr/net/Starter/Recruit/SS/engineering?rOrderTab=4#JobList', headers={'User-Agent':'Mozilla/5.0'})
     webpage = urlopen(html).read()
     soup = BeautifulSoup(webpage)
-    event = soup.find_all("a" ,class_="eventLink")
+    event = soup.find_all("a" ,class_="emp1")
     for i in event:
         print (i.get("title"))
 
