8feea8d89663a5e68f5455849f82d0979910994c
YongJang <yj_430@naver.com>
2016-05-02 00:50:21 +0900

    ee

--- examples/e.py
+++ examples/e.py
@@ -2,6 +2,6 @@ import feedparser
 
 d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=%C0%CE%B0%F8%C1%F6%B4%C9&field=0')
 
-print d['feed']['title']
+print (d['feed']['title'])
 
-print d['feed']['link']
+print (d['feed']['link'])
