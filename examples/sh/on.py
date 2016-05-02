42058b31e5f64b3c316d92a8f970b197c1b61b0a
alsqls <tpgml9247@gmail.com>
2016-05-03 00:56:00 +0900

    다아시

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -2,18 +2,25 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
+firsthtml = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=1#JobList',headers={'User-Agent':'Mozilla/5.0'})
+firstpage = urlopen(firsthtml).read()
+firstsoup = BeautifulSoup(firstpage)
+page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li')
+print (len(page_num))
 def getPost() :
     for page in range(1,page_num):
         html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + page + '  #JobList',headers={'User-Agent':'Mozilla/5.0'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage)
-
         info_title = soup.find_all("a" ,class_="emp1")
         print(len(info_title))
         tag = ["소프트웨어", "프로그래머"]
-        page_num = soup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li')
-        print (len(page_num))
-
+        for i in range(1,info_title):
+            for j in range(len(tag)) :
+                if i.get("title") is not None and tag[j] in i.get("title"):
+                    print (i.get("title"))
+                else:
+                    print ("nothing")
 
 def Medium_Technology() :
     getPost()
