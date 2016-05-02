ab1c822ebbbab0538a9df2835ef8e2ca77eea0ec
YongJang <yj_430@naver.com>
2016-05-02 16:40:55 +0900

    키워드 추가

--- examples/rssExample.py
+++ examples/rssExample.py
@@ -2,27 +2,28 @@
 import feedparser
 import urllib.parse
 
-keyword = "인공지능"
+keyword = ["인공지능","빅데이터","영화","경제"]
 
-d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=' + urllib.parse.quote(keyword.encode("utf-8")) + '&field=0')
+for(n in range(0,len(keyword)))
+    d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=' + urllib.parse.quote(keyword.encode("utf-8")) + '&field=0')
 
-print (d['feed']['title'])
+    print (d['feed']['title'])
 
-print (d['feed']['link'])
+    print (d['feed']['link'])
 
-print (d.feed.subtitle)
+    print (d.feed.subtitle)
 
-print (len(d['entries']))
+    print (len(d['entries']))
 
-print (d['entries'][0]['title'])
+    print (d['entries'][0]['title'])
 
-print (d.entries[0]['link'])
+    print (d.entries[0]['link'])
 
 
-for post in d.entries:
-    print (post.title + ":" + post.link + "\n")
-    print (post.published + ":" + post.summary + "\n")
+    for post in d.entries:
+        print (post.title + ":" + post.link + "\n")
+        print (post.published + ":" + post.summary + "\n")
 
-print (d.version)
+    print (d.version)
 
-print (d.headers.get('content-type'))
+    print (d.headers.get('content-type'))
