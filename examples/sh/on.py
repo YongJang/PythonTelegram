61aeb49b2b71189ab2e32aa57f752cb89921b5ee
alsqls <tpgml9247@gmail.com>
2016-05-02 23:31:22 +0900

    다시

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -10,18 +10,16 @@ def getPost() :
 
     page_num =  soup.find_all("div" ,class_="lgiSec lgiPagination lgiPagination1")
     info_title = soup.find_all("a" ,class_="emp1")
-    page = soup.find_all("div" ,class_="lgiSec lgiPagination lgiPagination1")
     tag = ["소프트웨어", "프로그래머"]
     for page in range(1,page_num):
    	    html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + page + '#JobList', headers={'User-Agent':'Mozilla/5.0'})
-
             for i in info_title:
                 for j in range(len(tag)):
                     if i.get("title") is not None and tag[j] in i.get("title"):
                         print (i.get("title"))
                     else:
                         print ("nothing")
-    
+
 def Medium_Technology() :
     getPost()
 
