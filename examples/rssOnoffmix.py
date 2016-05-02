b7590a6d5048339f3a4ff5ce389dcb6edcffe736
YongJang <yj_430@naver.com>
2016-05-02 17:10:15 +0900

    온오프 믹스

---
+++ examples/rssOnoffmix.py
@@ -0,0 +1,29 @@
+
+import feedparser
+import urllib.parse
+
+keyword = ["인공지능"]
+
+for n in range(0,len(keyword)):
+    d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=' + urllib.parse.quote(keyword[n].encode("utf-8")) + '&field=0')
+
+    print (d['feed']['title'])
+
+    print (d['feed']['link'])
+
+    print (d.feed.subtitle)
+
+    print (len(d['entries']))
+
+    print (d['entries'][0]['title'])
+
+    print (d.entries[0]['link'])
+
+
+    for post in d.entries:
+        print (post.title + ":" + post.link + "\n")
+        print (post.published + ":" + post.summary + "\n")
+
+    print (d.version)
+
+    print (d.headers.get('content-type'))
