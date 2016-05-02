c22a68bb8df6d199ca718cccced8a48cf99ef3ad
alsqls <tpgml9247@gmail.com>
2016-05-03 01:45:28 +0900

    href

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -12,15 +12,16 @@ def getPost() :
         html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + str(page) + '  #JobList',headers={'User-Agent':'Mozilla/5.0'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage)
-        info_title = soup.find_all("a" ,class_="emp1")
-        print(len(info_title))
+        info_title = soup.find_all("a" ,class_="emp1") #80
         tag = ["소프트웨어", "프로그래머","개발자"]
         for i in info_title:
             for j in range(len(tag)) :
                 if i.get("title") is not None and tag[j] in i.get("title"):
-                    print (i.get("href"))
+                    tag_href = i.get("href")
+                    print (tag_href)
                 else:
-                    print ("nothing")
+                    print ("nothing") # delete this
+
 
 def Medium_Technology() :
     getPost()
