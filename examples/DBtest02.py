91cf382673494b94b558280a7567820f337bac31
YongJang <yj_430@naver.com>
2016-05-03 20:46:10 +0900

    데이터 베이스 insert 테스트

--- examples/DBtest02.py
+++ examples/DBtest02.py
@@ -1,5 +1,7 @@
 import pymysql
 import sys
+import feedparser
+import urllib.parse
 
 try:
         print(sys.stdin.encoding)
@@ -10,7 +12,34 @@ try:
 
         cur = conn.cursor()
 
-        cur.execute("INSERT INTO article (url, tag, content, click_num, type, k_group, pDate) VALUES ();")
+
+
+        keyword = ["인공지능","빅데이터","영화","경제"]
+
+        for n in range(0,len(keyword)):
+            url = 'http://newssearch.naver.com/search.naver?where=rss&query=' + urllib.parse.quote(keyword[n].encode("utf-8")) + '&field=0'
+            d = feedparser.parse(url)
+
+            print (d['feed']['title'])
+
+            print (d['feed']['link'])
+
+            print (d.feed.subtitle)
+
+            print (len(d['entries']))
+
+            print (d['entries'][0]['title'])
+
+            print (d.entries[0]['link'])
+
+
+            for post in d.entries:
+                cur.execute("INSERT INTO article (url, tag, content, click_num, type, k_group, pDate) VALUES ('" + post.link +"','" + keyword[n] + "','" + post.summary + "', 0, 'IT', 0, '20160503');")
+
+            print (d.version)
+
+            print (d.headers.get('content-type'))
+
 
 except pymysql.Error as e:
         print ("Error %d: %s" % (e.args[0], e.args[1]))
