c90d8c53ed43cb4da7459769d2d37325f7075532
alsqls <tpgml9247@gmail.com>
2016-05-03 15:50:37 +0900

    set

--- examples/sh/job.py
+++ examples/sh/job.py
@@ -14,11 +14,13 @@ def getPost() :
         soup = BeautifulSoup(webpage)
         info_title = soup.find_all("a" ,class_="emp1") #80
         tag_href = []
-        tag = ["프로그래머", "개발", "소프트웨어"]
+        tag = ["프로그래머", "개발", "소프트웨어", "웹", "네트워크", "솔루션","머신", "C++", "C", "JAVA"]
         for i in info_title:
             for j in range(len(tag)):
                 if i.get("title") is not None and tag[j] in i.get("title"):
                     tag_href.append(i.get("href"))
+            tag_href = list(set(tag_href))
+            print(tag_href)
 
         for index in range(len(tag_href)):
             detail_html = Request('http://www.jobkorea.co.kr/' + str(tag_href[index]), headers={'User-Agent':'Mozilla/5.0'})
