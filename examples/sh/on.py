56e04e1817fd1f50a715a01d2d99e7043fd3850c
alsqls <tpgml9247@gmail.com>
2016-05-03 14:48:20 +0900

    ㅡㅡ

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -8,19 +8,19 @@ firstsoup = BeautifulSoup(firstpage)
 page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li')
 
 def getPost() :
-    for page in range(len(page_num)) :
+    for page in range(len(page_num)):
         html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + str(page) + '  #JobList', headers={'User-Agent':'Mozilla/5.0'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage)
         info_title = soup.find_all("a" ,class_="emp1") #80
         tag_href =[]
         tag = ["프로그래머", "개발", "H/W", "소프트웨어"]
-        for i in info_title :
-            for j in range(len(tag)) :
-                if i.get("title") is not None and tag[j] in i.get("title") :
+        for i in info_title:
+            for j in range(len(tag)):
+                if i.get("title") is not None and tag[j] in i.get("title"):
                     tag_href.append(i.get("href"))
 
-        for index in range(len(tag_href)) :
+        for index in range(len(tag_href)):
             detail_html = Request('http://www.jobkorea.co.kr/' + str(tag_href[index]), headers={'User-Agent':'Mozilla/5.0'})
             detailpage = urlopen(detail_html).read()
             detailsoup = BeautifulSoup(detailpage)
@@ -29,10 +29,10 @@ def getPost() :
 
             date = detailsoup.find_all("dl", class_="day")
             #date = detailsoup.find("dl",{ "class" : "day"}).find_all(text = True)
-            for d in date :
+            for d in date:
                 print(d.getText())
 
-def Medium_Technology() :
+def Medium_Technology():
     getPost()
 
 if __name__ == '__main__' :
