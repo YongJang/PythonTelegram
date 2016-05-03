aa460c1a6bd355f575d61fe1660d496fedfbb20b
YongJang <yj_430@naver.com>
2016-05-03 20:06:19 +0900

    1111

--- examples/DBtest01.py
+++ examples/DBtest01.py
@@ -10,7 +10,7 @@ try:
 
         cur = conn.cursor()
 
-        cur.execute("CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(200) UNIQUE KEY NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, type VARCHAR(40), k_group INT, pDate char(12));")
+        cur.execute("CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) UNIQUE KEY NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, type VARCHAR(40), k_group INT, pDate char(12));")
 
 except pymysql.Error as e:
         print ("Error %d: %s" % (e.args[0], e.args[1]))
