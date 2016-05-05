68dc73f82680d08ebf2e6bd62eac06e7850dc024
alsqls <tpgml9247@gmail.com>
2016-05-05 16:31:32 +0900

    ssssssssshhhhhhhhh

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -15,8 +15,9 @@ def getPost() :
         #title
         info_title = soup.find("div" ,  { "class" : "subjectNormal" }) #href 가져오기 40 개
         for t in info_title :
-            print(t.findChild(href=True).get('href'))
-
+            s = t.findChild(href=True)
+            if 'href' in s:
+                print (s.string)
 
         #tags = ["프로그래머", "개발", "소프트웨어","웹","S/W","H/W","솔루션"]
 
