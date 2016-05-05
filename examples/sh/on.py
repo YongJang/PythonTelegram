5c6846476935182fd13f2a94319aceace0e14938
alsqls <tpgml9247@gmail.com>
2016-05-06 00:21:50 +0900

    이거다이고!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- examples/sh/on.py
+++ examples/sh/on.py
@@ -11,7 +11,7 @@ page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPaginatio
 def getPost() :
     hrefs=[]  #href 가져오기 40 개
     for page in range(0,len(page_num)):
-        time.sleep(3)
+        time.sleep(3) #30*60 = 1800
         html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + str(page) + '  #JobList', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage)
@@ -21,18 +21,23 @@ def getPost() :
                 hrefs.append(t.get("href"))
 
         for index in range(0,len(hrefs)):
-            time.sleep(3)
+            time.sleep(3) #30*60 = 1800
             detail_html = Request('http://www.jobkorea.co.kr/' + str(hrefs[index]), headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
             detailpage = urlopen(detail_html).read()
             detailsoup = BeautifulSoup(detailpage)
             titles = detailsoup.find("span",{"class" : "title"})
             if titles is not None :
                 print (titles.text)
-            #date = detailsoup.find_all("dl", class_="day")
-            date = detailsoup.find("dl",{ "class" : "day"}).find_all("dd")
+            date = detailsoup.find_all("dl", class_="day")
             for d in date:
-                datetext = d.getText().replace('.', '')
-                print(datetext)
+                datetext = d.getText().strip()
+                deadline = datetext.replace('\n', ' ')
+                #시작일 : 2016.05.05(목) 마감일 : 2016.05.11(수)
+                year = deadline[26:30]
+                month = deadline[31:33]
+                day = deadline[34:36]
+                pDate = year + month + day
+                print(pDate)
 
 def Medium_Technology():
     getPost()
