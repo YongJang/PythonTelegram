18f51cab15bb47ef3e57e5c8b63e99d568d2c499
alsqls <tpgml9247@gmail.com>
2016-05-05 18:24:47 +0900

    ㅅㄷㄴㅅ

---
+++ examples/sh/test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import time
+
+firsthtml = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=1#JobList', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
+firstpage = urlopen(firsthtml).read()
+firstsoup = BeautifulSoup(firstpage)
+page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li') #page개수
+
+def getPost() :
+    hrefs=[]  #href 가져오기 40 개
+    for page in range(len(page_num)):
+        time.sleep(3)
+        html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + str(page) + '  #JobList', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
+        webpage = urlopen(html).read()
+        soup = BeautifulSoup(webpage)
+        info = soup.find_all("a" ,class_="emp1")
+        for t in info :
+            if t.get("href") is not None :
+                hrefs.append(t.get("href"))
+                print(hrefs)
+    print("!!!!!")
+
+
+
+def Medium_Technology():
+    getPost()
+
+if __name__ == '__main__' :
+    Medium_Technology()
