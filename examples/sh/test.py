35c8190908fd0dc83abdcd39793086379517e63b
alsqls <tpgml9247@gmail.com>
2016-05-07 03:13:11 +0900

    되라ㅏㅏ

--- examples/sh/test.py
+++ examples/sh/test.py
@@ -34,10 +34,10 @@ try:
                 #print(info.text)
                 #date = soup.find("dl",{ "class" : "day"}).find_all("dd")
                 date = soup.find_all("dl", class_="day")
-                keywords = soup.find_all("dt", text="키워드")
+                keywords = soup.find("dt", text="키워드").find_all("a")
                 #keywords = soup.find("div", {"class" : "keyword"}).find_all("dt"="키워드")
                 #soup.find_all('dt', text=False):
-                print(keywords.dd.ul.li.a.getText())
+                print(keywords.getText())
 
                 for d in date:
                     datetext = d.getText().strip()
