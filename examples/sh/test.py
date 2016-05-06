b3312d40286bcb4eafe5cdb5161d3238932a87cf
alsqls <tpgml9247@gmail.com>
2016-05-07 03:27:08 +0900

    findChildren()

--- examples/sh/test.py
+++ examples/sh/test.py
@@ -34,7 +34,7 @@ try:
                 #print(info.text)
                 #date = soup.find("dl",{ "class" : "day"}).find_all("dd")
                 date = soup.find_all("dl", class_="day")
-                keywords = soup.find("dt", text="키워드").find("dd").find("ui")
+                keywords = soup.find("dt", text="키워드").findChildren()
                 print(keywords)
                 #keywords = soup.find("div", {"class" : "keyword"}).find_all("dt"="키워드")
                 #soup.find_all('dt', text=False):
