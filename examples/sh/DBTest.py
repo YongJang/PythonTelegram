f633b6279dac73ad65bd46f87a90ac189533560e
alsqls <tpgml9247@gmail.com>
2016-05-07 22:57:25 +0900

    ㅇㅇㅇ

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -51,12 +51,12 @@ try:
                     print (titles.text)
 
                 date = detailsoup.find_all("dl", class_="day")
-
+                '''
                 keyword = detailsoup.find("dt", text = "키워드").next_element.next_element.next_element.find_all("a", href = True , target ="_top")
                 if keyword is not None :
                     for k in keyword :
                         print(k.getText())
-
+                '''
 
                 for d in date:
                     datetext = d.getText().strip()
