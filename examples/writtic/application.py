83a763d55aa077c3e954a58adc5c890ce0ce18d6
writtic <writtic@gmail.com>
2016-05-07 18:37:38 +0900

    라인 정렬

--- examples/writtic/application.py
+++ examples/writtic/application.py
@@ -8,20 +8,20 @@ application.debug = True
 
 @application.route('/')
 def hello_world():
-  storage = Storage()
-  storage.populate()
-  score = storage.score()
-  return "Are you OK %d!" % score
+    storage = Storage()
+    storage.populate()
+    score = storage.score()
+    return "Are you OK %d!" % score
 
 class Storage():
-  def __init__(self):
-    self.db = pymysql.connect(
-        user = os.getenv('MYSQL_USERNAME', 'yongjang'),
-        passwd = os.getenv('MYSQL_PASSWORD', 'yongjang'),
-        db = os.getenv('MYSQL_INSTANCE_NAME', 'telegramdb'),
-        host = os.getenv('MYSQL_PORT_3306_TCP_ADDR', 'telegramdb.cctjzlx6kmlc.ap-northeast-1.rds.amazonaws.com'),
-        port = int(os.getenv('MYSQL_PORT_3306_TCP_PORT', '3306'))
-      )
+    def __init__(self):
+        self.db = pymysql.connect(
+            user = os.getenv('MYSQL_USERNAME', 'yongjang'),
+            passwd = os.getenv('MYSQL_PASSWORD', 'yongjang'),
+            db = os.getenv('MYSQL_INSTANCE_NAME', 'telegramdb'),
+            host = os.getenv('MYSQL_PORT_3306_TCP_ADDR', 'telegramdb.cctjzlx6kmlc.ap-northeast-1.rds.amazonaws.com'),
+            port = int(os.getenv('MYSQL_PORT_3306_TCP_PORT', '3306'))
+            )
 
     cur = self.db.cursor()
     cur.execute("set names utf8")
@@ -29,15 +29,15 @@ class Storage():
     #cur.execute("CREATE TABLE scores(score INT)")
 
 
-  def populate(self):
-    cur = self.db.cursor()
-    cur.execute("INSERT INTO scores(score) VALUES(520)")
+    def populate(self):
+        cur = self.db.cursor()
+        cur.execute("INSERT INTO scores(score) VALUES(520)")
 
-  def getArticle(self):
-    cur = self.db.cursor()
-    cur.execute("SELECT * FROM article")
-    row = cur.fetchall()
-    return row
+    def getArticle(self):
+        cur = self.db.cursor()
+        cur.execute("SELECT * FROM article")
+        row = cur.fetchall()
+        return row
 
 if __name__ == "__main__":
-  application.run(host='0.0.0.0', port=3300)
+    application.run(host='0.0.0.0', port=3300)
