3f623760902fd2dc6a23bbc7b2cd269d6c43948a
YongJang <yj_430@naver.com>
2016-05-07 20:21:31 +0900

    dd

--- articleTableQuery.sql
+++ articleTableQuery.sql
@@ -3,4 +3,6 @@ drop table article;
 CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, aType VARCHAR(40), k_group INT, pDate char(8))default character set utf8;
 INSERT INTO article (url, tag, content, click_num, aType, k_group, pDate) VALUES ('http://www.asiatoday.co.kr/view.php?key=20160503002218366','인공지능','이세돌 9단은 지난달 인공지능 `알파고`와 대결한 이후 6전 전승 무패 행진도 이어갔다. 이날 승리로 이세돌 9단은 원성진 9단에게 상대 전적 14승 11패로 더욱 앞서나갔다. 원성진 9단은 2국과 오는 18일 3국을 모두...',0,'IT',0,'20160503');
 select * from article;
-CREATE TABLE tags(high varchar(40), mid varchar(40), bottom varchar(40))
+CREATE TABLE tags(high varchar(40), mid varchar(40), bottom varchar(40));
+drop table tags;
+CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) NOT NULL, high VARCHAR(40), low VARCHAR(40), title TEXT, content TEXT, click_num INT, aType VARCHAR(40), k_group INT, pDate char(8))default character set utf8;
