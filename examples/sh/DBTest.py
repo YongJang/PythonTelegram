28794e76bfe50e8a17683df8af4e8588852a0c67
alsqls <tpgml9247@gmail.com>
2016-05-07 16:12:31 +0900

    , from_encoding="utf-8"

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -20,7 +20,7 @@ try:
         firsthtml = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=1#JobList', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         sleep_i = sleep_i + 1
         firstpage = urlopen(firsthtml).read()
-        firstsoup = BeautifulSoup(firstpage)
+        firstsoup = BeautifulSoup(firstpage , from_encoding="utf-8")
         page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li') #page개수
 
 
@@ -30,7 +30,7 @@ try:
             html = Request('http://www.jobkorea.co.kr/Starter/Recruit/SS/engineering?psTab=40&rOrderTab=10&Page=' + str(page) + '  #JobList', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
             sleep_i = sleep_i + 1
             webpage = urlopen(html).read()
-            soup = BeautifulSoup(webpage)
+            soup = BeautifulSoup(webpage , from_encoding="utf-8")
             info = soup.find_all("a" ,class_="emp1")
             for t in info :
                 if t.get("href") is not None :
@@ -45,7 +45,7 @@ try:
                 detail_html = Request('http://www.jobkorea.co.kr/' + str(hrefs[index]), headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
                 sleep_i = sleep_i + 1
                 detailpage = urlopen(detail_html).read()
-                detailsoup = BeautifulSoup(detailpage)
+                detailsoup = BeautifulSoup(detailpage , from_encoding="utf-8")
                 titles = detailsoup.find("span",{"class" : "title"})
                 if titles is not None :
                     print (titles.text)
