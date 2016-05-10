09184ce596cb3a6748a0953448f81328993b6a82
YongJang <yj_430@naver.com>
2016-05-11 02:07:29 +0900

    aaaa

--- examples/article_bot.py
+++ examples/article_bot.py
@@ -47,14 +47,38 @@ CUSTOM_KEYBOARD = [
         [CMD_USER],
         ]
 
-keyboard = CUSTOM_KEYBOARD
+# 메시지 발송 관련 함수들
+def send_msg(chat_id, text, reply_to=None, no_preview=True, keyboard=None):
+    u"""send_msg: 메시지 발송
+    chat_id:    (integer) 메시지를 보낼 채팅 ID
+    text:       (string)  메시지 내용
+    reply_to:   (integer) ~메시지에 대한 답장
+    no_preview: (boolean) URL 자동 링크(미리보기) 끄기
+    keyboard:   (list)    커스텀 키보드 지정
+    """
+    params = {
+        'chat_id': str(chat_id),
+        'text': text.encode('utf-8'),
+        }
+    if reply_to:
+        params['reply_to_message_id'] = reply_to
+    if no_preview:
+        params['disable_web_page_preview'] = no_preview
+    if keyboard:
+        reply_markup = json.dumps({
+            'keyboard': keyboard,
+            'resize_keyboard': True,
+            'one_time_keyboard': False,
+            'selective': (reply_to != None),
+            })
+        params['reply_markup'] = reply_markup
+    try:
+        urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
+    except Exception as e:
+        logging.exception(e)
 
-reply_markup = json.dumps({
-    'keyboard': keyboard,
-    'resize_keyboard': True,
-    'one_time_keyboard': False,
-    'selective': (reply_to != None),
-    })
+
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -80,6 +104,82 @@ def echo_message(message):
         bot.send_message(chat_id, "button", keyboard = CUSTOM_KEYBOARD)
     else:
         bot.reply_to(message, message.text)
+
+
+u"""
+=============================================================================================================
+"""
+
+# 봇 명령 처리 함수들
+def cmd_start(chat_id):
+    u"""cmd_start: 봇을 활성화하고, 활성화 메시지 발송
+    chat_id: (integer) 채팅 ID
+    """
+    send_msg(chat_id, MSG_START, keyboard=CUSTOM_KEYBOARD)
+
+def cmd_stop(chat_id):
+    u"""cmd_stop: 봇을 비활성화하고, 비활성화 메시지 발송
+    chat_id: (integer) 채팅 ID
+    """
+    send_msg(chat_id, MSG_STOP)
+
+def cmd_help(chat_id):
+    u"""cmd_help: 봇 사용법 메시지 발송
+    chat_id: (integer) 채팅 ID
+    """
+    send_msg(chat_id, USAGE, keyboard=CUSTOM_KEYBOARD)
+
+def cmd_broadcast(chat_id, text):
+    u"""cmd_broadcast: 봇이 활성화된 모든 채팅에 메시지 방송
+    chat_id: (integer) 채팅 ID
+    text:    (string)  방송할 메시지
+    """
+    send_msg(chat_id, u'메시지를 방송합니다.', keyboard=CUSTOM_KEYBOARD)
+    broadcast(text)
+
+def cmd_echo(chat_id, text, reply_to):
+    u"""cmd_echo: 사용자의 메시지를 따라서 답장
+    chat_id:  (integer) 채팅 ID
+    text:     (string)  사용자가 보낸 메시지 내용
+    reply_to: (integer) 답장할 메시지 ID
+    """
+    send_msg(chat_id, text, reply_to=reply_to)
+
+def process_cmds(msg):
+    u"""사용자 메시지를 분석해 봇 명령을 처리
+    chat_id: (integer) 채팅 ID
+    text:    (string)  사용자가 보낸 메시지 내용
+    """
+    msg_id = msg['message_id']
+    chat_id = msg['chat']['id']
+    text = msg.get('text')
+    if (not text):
+        return
+    if CMD_START == text:
+        cmd_start(chat_id)
+        return
+        u"""
+    if (not get_enabled(chat_id)):
+        return
+        """
+    if CMD_STOP == text:
+        cmd_stop(chat_id)
+        return
+    if CMD_HELP == text:
+        cmd_help(chat_id)
+        return
+    cmd_broadcast_match = re.match('^' + CMD_BROADCAST + ' (.*)', text)
+    if cmd_broadcast_match:
+        cmd_broadcast(chat_id, cmd_broadcast_match.group(1))
+        return
+    cmd_echo(chat_id, text, reply_to=msg_id)
+    return
+
+
+
+u"""
+========================================================================================================================
+"""
 
 
 @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
