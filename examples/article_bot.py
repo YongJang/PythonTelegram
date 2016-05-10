f5c9b9d9cc602bd4b57f0f271344b8f1f129f6ac
YongJang <yj_430@naver.com>
2016-05-11 02:23:02 +0900

    request

--- examples/article_bot.py
+++ examples/article_bot.py
@@ -73,7 +73,7 @@ def send_msg(chat_id, text, reply_to=None, no_preview=True, keyboard=None):
             })
         params['reply_markup'] = reply_markup
     try:
-        urllib.parse.request(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
+        urllib.request(BASE_URL + 'sendMessage', urllib.urlencode(params)).read()
     except Exception as e:
         logging.exception(e)
 
