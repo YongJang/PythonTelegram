7af62e1ed48fc9d2e1dae0a6d789adbdd178290f
YongJang <yj_430@naver.com>
2016-05-11 02:17:22 +0900

    aa

--- examples/article_bot.py
+++ examples/article_bot.py
@@ -152,7 +152,7 @@ def process_cmds(msg):
     chat_id: (integer) 채팅 ID
     text:    (string)  사용자가 보낸 메시지 내용
     """
-    msg_id = msg.gessage_id
+    msg_id = msg.message_id
     chat_id = msg.chat.id
     text = msg.get('text')
     if (not text):
