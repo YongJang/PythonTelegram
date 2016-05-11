9043c43fd696e8087506f5b8f8f834a98c095856
alsqls <tpgml9247@gmail.com>
2016-05-11 16:37:29 +0900

    testttt

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -65,12 +65,14 @@ try:
                         for k_count in range(len(k_list)) :
                             result = k_list.count(k_list[k_count]) # 숫자세기
                             #print(result)
-                            if cur.execute("""SELECT * from tags where low = %s""", k_list ) > 0 :
+                            if cur.execute("SELECT * from tags where low = '%s'", k_list ) > 0 :
                                 db_tags.append(k_list) # 통신,15,네트워크,15
                                 tag_str = ""
                                 for n in range(len(db_tags)) :
+                                    print(n.text)
                                     num = ',15,'
-                                    tag_str = n.text + num
+                                    #tag_str = n.text + num
+
 
                     tag_str = tag_str[:-1]
 
