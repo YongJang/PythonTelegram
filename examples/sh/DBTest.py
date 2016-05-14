bc347e5d7cf5833113295bfc76dc99bb49c4063a
alsqls <tpgml9247@gmail.com>
2016-05-14 16:12:58 +0900

    ddddd

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -62,16 +62,16 @@ try:
 
                         for k_count in range(len(k_list)) :
                             result = k_list.count(k_list[k_count]) # 숫자세기
-        
+
                             if cur.execute("""SELECT * from tags where low = %s""", str(k_list[k_count])) > 0 :
                                 db_tags.append(k_list[k_count]) # low == tags
 
-                                for n in range(len(db_tags)) :
-                                    num = ",15," # 가중치
-                                    tag_str = tag_str + str(db_tags[n]) + num # 통신,15,네트워크,15
-                                tag_str = tag_str[:-1]
-                            print(tag_str)
-                            print(len(tag_str))
+                        for n in range(len(db_tags)) :
+                            num = ",15," # 가중치
+                            tag_str = tag_str + str(db_tags[n]) + num # 통신,15,네트워크,15
+                        tag_str = tag_str[:-1]
+                        print(tag_str)
+                        print(len(tag_str))
                     #<p class="regular">2016.05.12(목) ~  2016.07.31(일)</p>
                     if calendar is not None :
                         for d in calendar:
