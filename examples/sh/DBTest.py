ac56799d4ae2af3543fe97d175e6ddb5a6f22b5b
alsqls <tpgml9247@gmail.com>
2016-05-14 16:44:14 +0900

    del db_tags[:]

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -94,10 +94,10 @@ try:
 
                         if cur.execute("""SELECT url from job where url = %s""", 'http://www.jobkorea.co.kr/' + str(hrefs[index])) < 1 and  len(tag_str) > 0:
                             cur.execute("INSERT INTO job (url, high , low , title, content, click_num, aType, k_group, pDate) VALUES (\'http://www.jobkorea.co.kr/" + str(hrefs[index])  +"\',\' IT \',\'" + str(tag_str) + "\',\'"+ str(db_title) + "\' ,\' contents \' , 0, \'Job\', 0, \'" + pDate + "\');")
-                            db_tags[:]= db_tags[]
+                            del db_tags[:]
                             tag_str = ""
                         else :
-                            db_tags[:] = db_tags[]
+                            del db_tags[:]
                             tag_str = ""
                             continue
 
