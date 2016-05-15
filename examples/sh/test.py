27d6cb0a445a8377f3b984d2273cc8dfc89f0a84
alsqls <tpgml9247@gmail.com>
2016-05-15 19:59:07 +0900

    test date

--- examples/sh/test.py
+++ examples/sh/test.py
@@ -23,17 +23,16 @@ try:
         page_num = firstsoup.find("div" , { "class" : "lgiSec lgiPagination lgiPagination1" }).find_all('li') #page개수
 
         def getPost() :
-            i = 17001522
-            hrefs=[]  #href 가져오기 40 개
             for page in range(1,4):
                 time.sleep(1)
-                html = Request('http://www.jobkorea.co.kr/Recruit/GI_Read/' + str(i) + '?Oem_Code=C1&rPageCode=ST&PageGbn=ST', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
+                html = Request('http://www.jobkorea.co.kr/Recruit/GI_Read/17067787?Oem_Code=C1&rPageCode=ST&PageGbn=ST', headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
                 webpage = urlopen(html).read()
                 soup = BeautifulSoup(webpage, from_encoding="utf-8")
                 info = soup.find_all(class_="title")
                 #print(info.text)
                 #date = soup.find("dl",{ "class" : "day"}).find_all("dd")
-                date = soup.find_all("dl", class_="day")
+                #<p class="regular">2016.05.11(수) ~  2016.07.30(토)</p>
+                date = soup.find_all("p", class_="regular")
                 keywords = soup.find("dt", text = "키워드").next_element.next_element.next_element.find_all("a", href = True , target ="_top")
                 for k in keywords :
                     print(k.getText())
@@ -42,9 +41,9 @@ try:
                     datetext = d.getText().strip()
                     deadline = datetext.replace('\n', ' ')
                     #시작일 : 2016.05.05(목) 마감일 : 2016.05.11(수)
-                    year = deadline[26:30]
-                    month = deadline[31:33]
-                    day = deadline[34:36]
+                    year = deadline[19:23]
+                    month = deadline[24:26]
+                    day = deadline[27:29]
                     pDate = year + month + day
                     print(pDate)
 
