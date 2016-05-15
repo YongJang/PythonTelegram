63f7ffc87bf9bec7f3d2fdd9496a091a4ab324d2
YongJang <yj_430@naver.com>
2016-05-16 00:45:38 +0900

    이게 문제였어

--- examples/article_bot2.py
+++ examples/article_bot2.py
@@ -192,11 +192,13 @@ def command_default(m):
     bot.send_message(m.chat.id, "이게 무슨말?? \"" + m.text + "\"\nMaybe try the help page at /help")
 
 # 여기서 부터 callback_query 핸들러
-@bot.callback_query_handler(func=lambda call: call.data == "IT")
+@bot.callback_query_handler(func=lambda call: call.data == "IT", get_user_step(call.from_user.id) == 100)
+def step100IT(call):
     bot.answer_callback_query(call.id, text="IT 기사!!")
     userStep[cid] = 0
 
-@bot.callback_query_handler(func=lambda call: call.data == "사회")
+@bot.callback_query_handler(func=lambda call: call.data == "사회", get_user_step(call.from_user.id) == 100)
+def step100Social(call):
     bot.answer_callback_query(call.id, text="사회 기사!!")
     userStep[cid] = 0
 bot.polling()
