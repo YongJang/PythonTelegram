f1cb2192ad150612a56fbd1fa690e7578e8baf3f
alsqls <tpgml9247@gmail.com>
2016-05-17 03:12:46 +0900

    크

--- sh.sql
+++ sh.sql
@@ -1,15 +1,12 @@
 select version();
 use telegramdb;
 drop table article;
-CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, aType VARCHAR(40), k_group INT, pDate char(8))default character set utf8;
+CREATE TABLE testjob (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, aType VARCHAR(40), k_group INT, pDate char(8))default character set utf8;
 INSERT INTO article (url, tag, content, click_num, aType, k_group, pDate) VALUES ('http://www.asiatoday.co.kr/view.php?key=20160503002218366','인공지능','이세돌 9단은 지난달 인공지능 `알파고`와 대결한 이후 6전 전승 무패 행진도 이어갔다. 이날 승리로 이세돌 9단은 원성진 9단에게 상대 전적 14승 11패로 더욱 앞서나갔다. 원성진 9단은 2국과 오는 18일 3국을 모두...',0,'IT',0,'20160503');
-
-drop table job;
-
+select * from testjob;
 
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
 select * from tags;
-
 show global variables like 'c%'; 
 set character_set_client = utf8;
 set character_set_connection = utf8;
@@ -23,8 +20,12 @@ CREATE TABLE job(PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(100
 
 set character_set_connection=utf8;
 set names utf8;
+
 load data local infile 'E:\\PythonTelegram\\ITtags4.csv' into table tags fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
 load data local infile 'E:\\PythonTelegram\\SocialTags.csv' into table tags fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
 select * from job;
-drop table job;
+
+delete from job where aType= 'Job';
+
+SET SQL_SAFE_UPDATES=0;
