d6fdb17846c9dbfa40208e86c1a74bfb5ddac580
alsqls <tpgml9247@gmail.com>
2016-05-17 14:36:57 +0900

    ssss

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -69,13 +69,12 @@ try:
                                 db_tags.append(k_list[k_count]) # low == tags
 
                         for n in range(len(db_tags)) :
-                            #tag_str = tag_str + str(db_tags[n]) + "," + weight + "," # 통신,15,네트워크,15
                             tag_str = tag_str + "{" + str(db_tags[n]) + ":" + weight + "},"
-                            tag_str = json.dumps(tag_str, ensure_ascii=False, sort_keys=False)
+                            #tag_str = json.dumps(tag_str, ensure_ascii=False, sort_keys=False)
                         tag_str = tag_str[:-1]
                         db_tags.clear()
                         k_list.clear()
-                    #tag_str = json.dumps(tag_str , ensure_ascii=False, sort_keys=False)
+                    tag_str = json.dumps(tag_str , ensure_ascii=False, sort_keys=False)
                     print(tag_str)
                     print(len(db_tags))
                     #<p class="regular">2016.05.12(목) ~  2016.07.31(일)</p>
