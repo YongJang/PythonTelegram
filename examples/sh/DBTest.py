16a3065f0b62e78c0d5c7e16d2fc3d2cda2d5462
alsqls <tpgml9247@gmail.com>
2016-05-17 15:09:13 +0900

    rerere

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -71,13 +71,12 @@ try:
                         for n in range(len(db_tags)) :
                             tag_str = tag_str + "{" + str(db_tags[n]) + ":" + weight + "},"
                             #tag_str = json.dumps(tag_str, ensure_ascii=False, sort_keys=False)
-                        tag_str = tag_str[:-1]
-                        print(len(tag_str))
-                        db_tags.clear()
-                        k_list.clear()
+                    tag_str = tag_str[:-1]
+                    print(len(tag_str))
+                    db_tags.clear()
+                    k_list.clear()
                     tag_str = json.dumps(tag_str , ensure_ascii=False, sort_keys=False)
                     print(tag_str)
-                    print(len(db_tags))
                     #<p class="regular">2016.05.12(목) ~  2016.07.31(일)</p>
                     pDate = ""
                     if calendar  :
