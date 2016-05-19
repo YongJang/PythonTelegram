e3da573043e6fb4ad83c6ee3bea2102b952c7111
alsqls <tpgml9247@gmail.com>
2016-05-19 21:02:50 +0900

    qqq

--- sh.sql
+++ sh.sql
@@ -2,7 +2,7 @@ select version();
 use telegramdb;
 drop table article;
 CREATE TABLE testjob (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(1000) NOT NULL, tag VARCHAR(40), content TEXT, click_num INT, aType VARCHAR(40), k_group INT, pDate char(8))default character set utf8;
-INSERT INTO article (url, tag, content, click_num, aType, k_group, pDate) VALUES ('http://www.asiatoday.co.kr/view.php?key=20160503002218366','인공지능','이세돌 9단은 지난달 인공지능 `알파고`와 대결한 이후 6전 전승 무패 행진도 이어갔다. 이날 승리로 이세돌 9단은 원성진 9단에게 상대 전적 14승 11패로 더욱 앞서나갔다. 원성진 9단은 2국과 오는 18일 3국을 모두...',0,'IT',0,'20160503');
+INSERT INTO jobs (url, high , low , title, content, click_num, aType, k_group, pDate ,meta) VALUES ('http://www.asiatoday.co.kr/view.php?key=20160503002218366','IT','인공지능','이세돌',' 9단은 지난달 인공지능 `알파고`와 대결한 이후 6전 전승 무패 행진도 이어갔다. 이날 승리로 이세돌 9단은 원성진 9단에게 상대 전적 14승 11패로 더욱 앞서나갔다. 원성진 9단은 2국과 오는 18일 3국을 모두...',0,'IT',0,'20160503','<meta content=\"한국 바이켐 연구직 경력 및 신입사원 모집 - 잡코리아\" name=\"title\"/><meta content=\"한국 바이켐 연구직 경력 및 신입사원 모집 경력 : 신입·경력, 학력 : 대졸이상, 급여 : 2,500~3,000만원, 고용형태 : 정규직, 성별 : 남자, 나이 : , 마감일 : 2016.06.02\" name=\"description\"/>');
 select * from testjob;
 
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
@@ -24,6 +24,6 @@ load data local infile 'E:\\PythonTelegram\\SocialTags.csv' into table tags fiel
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
 select * from jobs;
 select * from information;
-
+delete from jobs where aType = 'Job';
 SET SQL_SAFE_UPDATES=0;
 desc jobs;
