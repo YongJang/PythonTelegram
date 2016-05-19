6f38a2e3733851d8a4213ce68ed490d4a0dbf058
alsqls <tpgml9247@gmail.com>
2016-05-19 18:07:45 +0900

    find all

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -57,7 +57,7 @@ try:
 
                     calendar = detailsoup.find_all("dl", class_="day") # 상세페이지의 마감일 찾기 (달력 형식)
                     date_second = detailsoup.find_all("p", class_="regular") # 다른 형식의 상세페이지의 마감일 (달력없는 형식)
-                    keyword = detailsoup.find('dt', text = '키워드').next_element.next_element.next_element.findAll("a", href = True , target ="_top") # 상세페이지의 키워드 찾기
+                    keyword = detailsoup.find('dt', text = '키워드').next_element.next_element.next_element.find_all("a", href = True , target ="_top") # 상세페이지의 키워드 찾기
                     print(keyword)
                     if keyword is not None :
                         weight = "15" # 가중치
