c7cf8f13693902598e89b0761de15053b9743040
alsqls <tpgml9247@gmail.com>
2016-05-19 20:24:18 +0900

    str4444

--- examples/sh/DBTest.py
+++ examples/sh/DBTest.py
@@ -105,7 +105,7 @@ try:
                         break
 
                     if cur.execute("""SELECT url from jobs where url = %s""", 'http://www.jobkorea.co.kr/' + str(hrefs[index])) < 1 and  len(tag_str) > 4:
-                        cur.execute("INSERT INTO jobs (url, high , low , title, content, click_num, aType, k_group, pDate, meta) VALUES (\'http://www.jobkorea.co.kr/" + str(hrefs[index])  +"\',\' IT \',\'[" + str(tag_str) + "]\',\'"+ str(db_title) + "\' ,\' contents \' , 0, \'Job\', 0, \'" + pDate + "\',\'"+ str(meta_all) + "\');")
+                        cur.execute("INSERT INTO jobs (url, high , low , title, content, click_num, aType, k_group, pDate, meta) VALUES (\'http://www.jobkorea.co.kr/" + str(hrefs[index])  +"\',\' IT \',\'[" + str(tag_str) + "]\',\'"+ str(db_title) + "\' ,\' contents \' , 0, \'Job\', 0, \'" + pDate + "\',\'"+ meta_all + "\');")
                         conn.commit()
                     else :
                         continue
