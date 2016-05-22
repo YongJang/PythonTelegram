47ba46d4c9fc83eb06d5693bc0c44a2eb940c7fb
YongJang <yj_430@naver.com>
2016-05-22 19:01:25 +0900

    aaa

--- examples/article_bot4.py
+++ examples/article_bot4.py
@@ -26,7 +26,7 @@ import sys
 print(sys.stdin.encoding)
 
 try:
-    conn = pymysql.connect(host='telegramdb.cctjzlx6kmlc.ap-northeast-1.rds.amazonaws.com', port=3306, user='yongjang', passwd='yongjang', db='telegramdb', charset='utf8')
+    conn = pymysql.connect(host='telegramdb.cctjzlx6kmlc.ap-northeast-1.rds.amazonaws.com', port=3306, user='yongjang', passwd='yongjang', db='telegramdb', charset='utf8', charset ='utf8', use_unicode=False, init_command='SET NAMES UTF8')
     print("Database Connection Success!!")
     cur = conn.cursor()
 except pymysql.Error as e:
@@ -248,7 +248,7 @@ def step110IT_1(call):
         print('No entries')
     else:
         for record in range(total):
-            temp = row[record][1].decode("utf-8")
+            temp = row[record][1].decode('utf8', 'surrogatepass')
             entriesURL.append(temp)
     #### entriesURL에 IT기사 URL 저장 ####
     isFirstShown = -1
