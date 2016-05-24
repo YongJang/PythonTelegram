5fa1e09a08c4950c6cb404c6becd2d26020eab99
YongJang <yj_430@naver.com>
2016-05-24 21:42:31 +0900

    sss

---
+++ examples/rssExample2.py
@@ -0,0 +1,31 @@
+
+import feedparser
+import urllib.parse
+
+keyword = ["인공지능","빅데이터","영화","경제"]
+
+for n in range(0,len(keyword)):
+    d = feedparser.parse('http://newssearch.naver.com/search.naver?where=rss&query=' + urllib.parse.quote(keyword[n].encode("utf-8")) + '&field=0')
+
+    print (d['feed']['title'])
+
+    print (d['feed']['link'])
+
+    print (d.feed.subtitle)
+
+    print (len(d['entries']))
+
+    print (d['entries'][0]['title'])
+
+    print (d.entries[0]['link'])
+
+
+    for post in d.entries:
+        print ("======== Entries =========")
+        print (post.title + ":" + post.link + "\n")
+        print (post.published + ":" + post.summary + "\n")
+
+
+    print (d.version)
+
+    print (d.headers.get('content-type'))
