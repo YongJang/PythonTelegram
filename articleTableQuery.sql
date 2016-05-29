8d451a5d5b367755659c1f9eeb8284e9337c5405
YongJang <yj_430@naver.com>
2016-05-29 19:57:31 +0900

    aa

--- articleTableQuery.sql
+++ articleTableQuery.sql
@@ -26,7 +26,7 @@ CREATE TABLE article (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varcha
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
 set character_set_connection=utf8;
 set names utf8;
-load data local infile 'C:\\Users\\Administrator\\Documents\\GitHub\\PythonTelegram\\ITtags4.csv' into table tags fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
+load data local infile 'D:\\Github\\PythonTelegram\\ITtags4.csv' into table tags fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
 load data local infile 'C:\\Users\\Administrator\\Documents\\GitHub\\PythonTelegram\\SocialTags.csv' into table tags fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
 CREATE TABLE tags (high varchar(40), low varchar(40))default character set utf8;
 select * from tags;
