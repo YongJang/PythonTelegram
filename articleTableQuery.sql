fdbfb69ac5e89517a68160d90ab77c0cf1040c7e
YongJang <yj_430@naver.com>
2016-05-31 23:06:12 +0900

    ALTER 대신 사용할것

--- articleTableQuery.sql
+++ articleTableQuery.sql
@@ -80,6 +80,7 @@ UPDATE society SET click_num = 0 WHERE click_num > 0;
 CREATE TABLE bot (name varchar(200))default character set utf8;
 SELECT * from bot;
 
+/* ALTER 대신 이걸 사용하자!!! */
 desc information;
 CREATE TABLE information_new (PK_aid INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, url varchar(250) UNIQUE KEY, high VARCHAR(20), low text, title varchar(200), content TEXT, click_num INT, a_type VARCHAR(20), k_group INT, p_date varchar(20), meta text)default character set utf8;
 INSERT INTO information_new (PK_aid, url, high, low, title, content, click_num, a_type, k_group, p_date, meta) SELECT * FROM information;
