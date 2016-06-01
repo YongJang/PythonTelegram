bb3b0902268b3baa62addeeabe650c296bbf55b2
YongJang <yj_430@naver.com>
2016-06-02 02:00:35 +0900

    테스트

--- examples/article_bot6.py
+++ examples/article_bot6.py
@@ -490,11 +490,12 @@ def step110IT_1(call):
         articleKeyboard.row(articleKeyboardDetail, articleKeyboardLink, articleKeyboardNext)
         articleKeyboard.row(KeywordButton3,KeywordButton4)
         longurl = WEBSERVER_DNS + "?url=" + str(aid) + "&tb=information&uid=" + str(cid)
+        longurl = "http://www.naver.com/"
         longurl = longurl.replace(":","%3A")
         longurl = longurl.replace("/","%2F")
         longurl = longurl.replace("?","%3F")
         longurl = longurl.replace("=","%3D")
-        
+
         #query_params = {'access_token': str(BITLY_API_KEY),
         #                'longUrl': str(longurl)}
 
