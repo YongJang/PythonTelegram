1a3f77b31d47b69fe42af739a779736466822db7
YongJang <yj_430@naver.com>
2016-06-02 01:15:58 +0900

    22

--- examples/article_bot6.py
+++ examples/article_bot6.py
@@ -44,7 +44,7 @@ WEBSERVER_DNS = 'TelegramRedirect-982942058.ap-northeast-1.elb.amazonaws.com/'
 BITLY_API_USER = 'yongjang'
 BITLY_API_KEY = 'R_2aa28870a1c440498cf13385c9fdaa16'
 
-bit = bitlyapi.BitLy(BITLY_API_USER, BITLY_API_KEY)
+bit = bitly_api.BitLy(BITLY_API_USER, BITLY_API_KEY)
 #if len(sys.argv) != 2:
 #    print ("bitLy 에러")
 #    sys.exit(0)
